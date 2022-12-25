6bc0696fa23ba85c55779b17d4f47e7fcbfb4406
Hamid <ali.hamid161999@gmail.com>
2022-12-25 18:19:05 +0530

    hamid

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -28,6 +28,31 @@ const Home = () => {
    
      <>
 <MetaData title= "Ecommerce" />
+
+
+<div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://rukminim1.flixcart.com/flap/50/50/image/75a15c3e19c3f7de.jpg?q=50" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://rukminim1.flixcart.com/fk-p-flap/50/50/image/474c69a2c8f12e5d.png?q=50" class="d-block w-100" alt="..."/>
+    </div>
+    <div class="carousel-item">
+      <img src="https://rukminim1.flixcart.com/fk-p-flap/844/140/image/a3573f74ec1e7359.jpeg?q=50" class="d-block w-100" alt="..."/>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+
 <div className="banner">
             <p>Ecommerce Website</p>
             <h1>APNA DUKAAN PRODUCTS</h1>
