dead44d64617416b6edbf12505d87baebeadc943
Hamid <ali.hamid161999@gmail.com>
2022-12-26 21:33:10 +0530

    bulid

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,27 @@ dotenv.config({
 
 // database
 connectDatabase();
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+//Routes go here
+app.all('*', (req,res) => {
+    res.json({"every thing":"is awesome"})
+})
+
+//Connect to the database before listening
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
 
 // fileupload cloudinary 
 
