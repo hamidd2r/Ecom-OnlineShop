dbaef43c16b2d43856bb527d5ba9ea3649ed98d1
Hamid <ali.hamid161999@gmail.com>
2023-01-09 10:38:22 +0530

    juli

--- backend/controllers/paymentController.js
+++ backend/controllers/paymentController.js
@@ -4,7 +4,7 @@ const orders = require("../models/OrderModel");
 //const instance = require("../server");
 const crypto = require("crypto");
 const shortid = require('shortid')
-const fs =  require('fs')
+const fs = require('fs')
 const Razorpay = require('razorpay');
 
 
@@ -70,18 +70,18 @@ exports.paymentVerification = catchAsyncErrors(async (req, res) => {
 
   if (isAuthenticated) {
     await Payment.create({
-      razorpay_order_id,  
+      razorpay_order_id,
       razorpay_payment_id,
-    razorpay_signature
-      
+      razorpay_signature
+
     })
-    res.redirect(`https://drab-crow-boot.cyclic.app//success?reference=${razorpay_payment_id}`)
+    res.redirect(`https://drab-crow-boot.cyclic.app/success?reference=${razorpay_payment_id}`)
   } else {
     res.status(200).json({
       success: true,
-      
-     
-      
+
+
+
     })
   }
 })
@@ -90,31 +90,31 @@ exports.paymentVerification = catchAsyncErrors(async (req, res) => {
 //......................
 exports.razorpay = catchAsyncErrors(async (req, res) => {
   const payment_capture = 1
-	const amount = 499
-	const currency = 'INR'
+  const amount = 499
+  const currency = 'INR'
 
-	const options = {
-		amount: amount * 100,
-		currency,
-		receipt: shortid.generate(),
-		payment_capture
-	}
+  const options = {
+    amount: amount * 100,
+    currency,
+    receipt: shortid.generate(),
+    payment_capture
+  }
 
 
-	try {
-		const response = await instance.orders.create(options)
-		console.log(response)
-		res.json({
-			id: response.id,
-			currency: response.currency,
-			amount: response.amount 
-		})
-  
-	} catch (error) {
-		console.log(error)
-	}
+  try {
+    const response = await instance.orders.create(options)
+    console.log(response)
+    res.json({
+      id: response.id,
+      currency: response.currency,
+      amount: response.amount
+    })
 
- 
+  } catch (error) {
+    console.log(error)
+  }
+
+
 })
 //..........................................
 
@@ -122,45 +122,57 @@ exports.razorpay = catchAsyncErrors(async (req, res) => {
 
 exports.getwebhookdata = catchAsyncErrors(async (req, res) => {
   // do a validation
-	const secret = 'allah123'
+  const secret = 'allah123'
 
-	console.log(req.body)
+  console.log(req.body)
 
-	const crypto = require('crypto')
+  const crypto = require('crypto')
 
-	const shasum = crypto.createHmac('sha256', secret)
-	shasum.update(JSON.stringify(req.body))
-	const digest = shasum.digest('hex')
+  const shasum = crypto.createHmac('sha256', secret)
+  shasum.update(JSON.stringify(req.body))
+  const digest = shasum.digest('hex')
 
 
-	console.log(digest, req.headers['x-razorpay-signature'])
+  console.log(digest, req.headers['x-razorpay-signature'])
 
-	if (digest === req.headers['x-razorpay-signature']) {
-		console.log('request is legit')
-		// process it
-		require('fs').writeFileSync('juli.json', JSON.stringify(req.body, null, 4))
-	} else {
-		// pass it
-	}
-	res.json({ status: 'ok' })
-  
+  if (digest === req.headers['x-razorpay-signature']) {
+    console.log('request is legit')
+    // process it
+    require('fs').writeFileSync('juli.json', JSON.stringify(req.body, null, 4))
+  } else {
+    // pass it
+  }
+  res.json({
+    status: 'ok'
+  })
+
 })
 
 
 exports.responseweb = catchAsyncErrors(async (req, res) => {
-   // STEP 1:
-   const {amount,currency,receipt, notes}  = req.body;      
-          
-   // STEP 2:    
-   options.instance.create({amount, currency, receipt, notes}, 
-       (err, order)=>{
-         
-         //STEP 3 & 4: 
-         if(!err)
-           res.json(order)
-         else
-           res.send(err);
-       }
-   )
-  
+  // STEP 1:
+  const {
+    amount,
+    currency,
+    receipt,
+    notes
+  } = req.body;
+
+  // STEP 2:    
+  options.instance.create({
+      amount,
+      currency,
+      receipt,
+      notes
+    },
+    (err, order) => {
+
+      //STEP 3 & 4: 
+      if (!err)
+        res.json(order)
+      else
+        res.send(err);
+    }
+  )
+
 })
--- backend/server.js
+++ backend/server.js
@@ -2,31 +2,35 @@ const app = require("./app");
 const cloudinary = require('cloudinary')
 const connectDatabase = require("./config/database");
 const cors = require("cors")
-const { MongoClient } = require('mongodb');
+const {
+  MongoClient
+} = require('mongodb');
 const dotenv = require('dotenv')
 
 const uri = process.env.MONGO_DB_URI;
 const client = new MongoClient(uri);
 
 app.get("/items/:my_item", async (req, res) => {
-    let my_item = req.params.my_item;
-    let item = await client.db("my_db")
-                .collection("my_collection")
-                .findOne({my_item: my_item})
+  let my_item = req.params.my_item;
+  let item = await client.db("my_db")
+    .collection("my_collection")
+    .findOne({
+      my_item: my_item
+    })
 
-    return res.json(item)
+  return res.json(item)
 })
 
 app.use(cors())
-const Stripe = require('strip')(process.env.SECRET_KEY) 
+const Stripe = require('strip')(process.env.SECRET_KEY)
 
 
 // handling uncaught exception
 
-process.on("uncaughtException" ,(err)=>{
-    console.log(`Error: ${err.message}`);
-    console.log(`shutting down the server due to uuncaught Exception `);
-    process.exit(1) 
+process.on("uncaughtException", (err) => {
+  console.log(`Error: ${err.message}`);
+  console.log(`shutting down the server due to uuncaught Exception `);
+  process.exit(1)
 })
 
 // Config
@@ -49,8 +53,10 @@ const connectDB = async () => {
 }
 
 //Routes go here
-app.all('*', (req,res) => {
-    res.json({"every thing":"is awesome"})
+app.all('*', (req, res) => {
+  res.json({
+    "every thing": "is awesome"
+  })
 })
 
 //Connect to the database before listening
@@ -59,9 +65,9 @@ app.all('*', (req,res) => {
 // fileupload cloudinary 
 
 cloudinary.config({
-  cloud_name:process.env.CLOUDINARY_NAME,
-  api_key:process.env.CLOUDINARY_API_KEY,
-  api_secret:process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
 const server = app.listen(process.env.PORT, () => {
@@ -79,5 +85,3 @@ process.on("unhandledRejection", (err) => {
     process.exit(1);
   });
 });
-
-
